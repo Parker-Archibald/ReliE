723961a6cb4e115897ed6badc358ac56a3622db0
Parker <parkerarchibald97@gmail.com>
2019-10-31 12:17:17 -0600

    initial commit with calculator page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Calculator from '../src/Pages/Calculator';
+
+document.title = 'ReliEnergy';
+
+function App() {
+  return (
+    <div>
+      <Calculator/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Calculator.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+import '../Styles/Calculator.css';
+
+let today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0');
+let yyyy = today.getFullYear() - 2000;
+today = `${dd}/${mm}/${yyyy}`
+
+class Calculator extends Component {
+    render() {
+        return (
+            <div>
+                <h1 id='calculatorTitle'>RELI ENERGY SOLUTIONS</h1>
+                <div id='topInfo'>
+                    <div id='squareFoot'>Square footage: </div>
+                    <div id='RValue'>Pre R Value: </div>
+                    <div id='year'>Year: </div>
+                </div>
+                <div id='additionalCosts'>
+                    <div id='originalCost'>Original Cost</div>
+                    <input id='originalCostInput'/> 
+                    <input id='originalCostInput1'/>
+                    <div id='CAZ'>CAZ Testing</div>
+                    <input id='input1'/>
+                    <input id='input2'/>
+                    <div id='energyAudit'>Energy Audit</div>
+                    <input id='input1'/>
+                    <input id='input2'/>
+                    <div id='airSeal'>Air Seal</div>
+                    <input id='input1'/>
+                    <input id='input2'/>
+                    <div id='ventilation'>Ventilation</div>
+                    <input id='input1'/>
+                    <input id='input2'/>
+                    <div id='rebate'>Rebate</div>
+                    <input id='input1'/>
+                    <input id='input2'/>
+                    <div id='other'>Other</div>
+                    <input id='input1'/>
+                    <input id='input2'/>
+                    <div id='other1'>Other</div>
+                    <input id='input1'/>
+                    <input id='input2'/>
+                </div>
+                <div id='atInstall'>
+                    <div id='costAtInstall'>Cost At Install</div>
+                    <input id='originalCostInput'/> 
+                    <input id='originalCostInput1'/>
+                    <div id='rebateAtInstall'>Rebate</div>
+                    <input id='input1'/>
+                    <input id='input2'/>
+                    <div id='taxCredit'>Tax Credit</div>
+                    <input id='input1'/>
+                    <input id='input2'/>
+                    <div id='totalInvestment'>Total Investment</div>
+                    <input id='originalCostInput'/> 
+                    <input id='originalCostInput1'/>
+                </div>
+                <div id='date'>Date: {today} </div>
+                <div id='smallText'>*This quoted cost is subject to changeuntil written on an official invoice. Cost may change based on available funding fromany incentive provider.</div>
+                <div id='bottomOfPage'>
+                    <div id='siteName'>www.relienergysolutions.com</div>
+                </div>
+                <div>
+                    test
+                </div>
+            </div>
+        )
+    }
+}
+export default Calculator;
