e652d3a23424e49b8d46e7b03f0d87a66329a4e5
Parker <parkerarchibald97@gmail.com>
2019-11-06 18:18:53 -0700

    fixed quotes and side nav error

--- src/config/Nav.js
+++ src/config/Nav.js
@@ -24,19 +24,19 @@ class Nav extends Component {
         return(
             <div>
                 <h1 id='calculatorTitle'><meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-                <div class="container" onClick={this.openBar} onMouseLeave={this.closeBar}>
+                <div class="container" onClick={this.openBar}>
                     <div id='bar1' className="bar1"></div>
                     <div id='bar2' className="bar2"></div>
                     <div id='bar3' className="bar3"></div>
                 </div>
                 RELI ENERGY SOLUTIONS
                 </h1>
-                <div id='leftNav' className='leftNav'>
-                    <Link to='/profile'><button id='profile'>Profile</button></Link>
-                    <Link to='/'><button id='dashboard'>Dashboard</button></Link>
-                    <Link to='/orders'><button id='orders'>Orders</button></Link>
-                    <Link to='/quotes'><button id='quotes'>Quotes</button></Link>
-                    <Link to='/paperwork'><button id='paperwork'>Paperwork</button></Link>
+                <div id='leftNav' className='leftNav' onblue={this.closeBar}>
+                    <div onClick={this.closeBar}><Link to='/profile'><button id='profile'>Profile</button></Link></div>
+                    <div onClick={this.closeBar}><Link to='/'><button id='dashboard'>Dashboard</button></Link></div>
+                    <div onClick={this.closeBar}><Link to='/orders'><button id='orders'>Orders</button></Link></div>
+                    <div onClick={this.closeBar}><Link to='/quotes'><button id='quotes'>Quotes</button></Link></div>
+                    <div onClick={this.closeBar}><Link to='/paperwork'><button id='paperwork'>Paperwork</button></Link></div>
                     <button id='signout' onClick={this.logout}>Signout</button>
                 </div>
                 <div id='bottomOfPage'>
