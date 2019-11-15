91fe8d5b4b3d45eea71c89940b3aa5ec8cd28304
Parker <parkerarchibald97@gmail.com>
2019-11-15 14:18:19 -0700

    added logout on nav

--- src/config/NewNav.js
+++ src/config/NewNav.js
@@ -35,6 +35,7 @@ class NewNav extends Component {
               <Link to='/orders'><li><a>Orders</a></li></Link>
               <Link to='/quotes'><li><a>Quotes</a></li></Link>
               <Link to='/paperwork'><li><a>Paperwork</a></li></Link>
+              <li onClick={this.logout}><a>Signout</a></li>
             </ul>
            </div>
           </nav>
